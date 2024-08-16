8a91bbcd60b67751532a9d50f54f2946bc46f5d1
root <root@NTO-LAP-053.nitcoinc.in>
2024-08-16 14:46:16 +0530

    updates

--- Backend-Java-Spring/ShopClues/src/main/java/com/Task/ShopClues/Service/CosmosDbConfig.java
+++ Backend-Java-Spring/ShopClues/src/main/java/com/Task/ShopClues/Service/CosmosDbConfig.java
@@ -27,6 +27,16 @@ public class CosmosDbConfig {
 
     @Bean
     public CosmosAsyncClient cosmosAsyncClient() {
+        // Add version-checking code here
+        String version = System.getProperty("java.version");
+        if (version.contains("-")) {
+            version = version.split("-")[0];
+        }
+        int javaVersion = Integer.parseInt(version);
+
+        // Use the parsed version if needed or simply log it
+        System.out.println("Java Version: " + javaVersion);
+
         return new CosmosClientBuilder()
                 .endpoint(endpoint)
                 .key(key)
@@ -59,5 +69,4 @@ public class CosmosDbConfig {
             ObjectMapper objectMapper) {
         return new MappingCosmosConverter(cosmosMappingContext, objectMapper);
     }
-
 }
